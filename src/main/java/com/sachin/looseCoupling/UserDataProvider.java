a59a66a8e08167f644da0d5cdf750063cfd65b72
Sachin Sharma <codwork.tdotslash@gmail.com>
2026-04-21 05:42:06 +0530

    add: learn about the Loose Coupling, and why and how it gives the scalability, flexibility and maintainability power

---
+++ src/main/java/com/sachin/looseCoupling/UserDataProvider.java
@@ -0,0 +1,5 @@
+package com.sachin.looseCoupling;
+
+public interface UserDataProvider {
+    String getUserDetails();
+}
---
+++ src/main/java/com/sachin/looseCoupling/WebServiceDatabaseProvider.java
@@ -0,0 +1,11 @@
+package com.sachin.looseCoupling;
+
+public class WebServiceDatabaseProvider implements UserDataProvider{
+    //We use custom logic here to get data from the External API e.g. REST API, microservices
+
+    @Override
+    public String getUserDetails() {
+        return "From webServiceDatabaseProvider: These are the user-details after interacting with the DB";
+
+    }
+}
